3c139333df74c595c0257cc13f2eb2cb034e0e23
Vladislav <110658464+flajoke@users.noreply.github.com>
2024-01-30 05:05:08 +0700

    added normal choose for subjects

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ BOT_USERNAME: Final = '@nstu_works_bot'
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [
-            InlineKeyboardButton("1 курс", callback_data='folder_1'),
-            InlineKeyboardButton("2 курс", callback_data='folder_2'),
-            InlineKeyboardButton("3 курс", callback_data='folder_3')
+            InlineKeyboardButton("1 курс", callback_data='kurs_1'),
+            InlineKeyboardButton("2 курс", callback_data='kurs_2'),
+            InlineKeyboardButton("3 курс", callback_data='kurs_3')
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -21,39 +21,72 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
     data = query.data
 
-    if data == 'folder_1':
+    if data == 'kurs_1':    
         keyboard = [
-            [InlineKeyboardButton("предмет фамилия 1", callback_data='subfolder_1_1')],
-            [InlineKeyboardButton("предмет фамилия 2", callback_data='subfolder_1_2')],
-            # ... добавьте столько подкатегорий, сколько вам нужно
+            [InlineKeyboardButton("Первый семестр", callback_data='sem_1')],
+            [InlineKeyboardButton("Второй семестр", callback_data='sem_2')],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_text(text="Выбрана Папка 1. Выберите подкатегорию:", reply_markup=reply_markup)
-    elif data == 'folder_2':
+        await query.edit_message_text(text="Выберите семестр:", reply_markup=reply_markup)
+    elif data == 'kurs_2':
         keyboard = [
-            [InlineKeyboardButton("предмет фамилия 1", callback_data='subfolder_2_1')],
-            [InlineKeyboardButton("предмет фамилия 2", callback_data='subfolder_2_2')],
-            # ... добавьте столько подкатегорий, сколько вам нужно
+            [InlineKeyboardButton("Третий семестр", callback_data='sem_3')],
+            [InlineKeyboardButton("Четвертый семестр", callback_data='sem_4')],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_text(text="Выбрана Папка 2. Выберите подкатегорию:", reply_markup=reply_markup)
-    elif data == 'folder_3':
+        await query.edit_message_text(text="Выберите семестр:", reply_markup=reply_markup)
+    elif data == 'kurs_3':
         keyboard = [
-            [InlineKeyboardButton("предмет фамилия 1", callback_data='subfolder_3_1')],
-            [InlineKeyboardButton("предмет фамилия 2", callback_data='subfolder_3_2')],
-            # ... добавьте столько подкатегорий, сколько вам нужно
+            [InlineKeyboardButton("Пятый семестр", callback_data='sem_5')],
+            #[InlineKeyboardButton("Шестой семестр", callback_data='sem_6')],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_text(text="Выбрана Папка 3. Выберите подкатегорию:", reply_markup=reply_markup)
+        await query.edit_message_text(text="Выберите семестр:", reply_markup=reply_markup)
     # Обработка выбора подкатегорий
-    elif data.startswith('subfolder_'):
-        # Здесь логика для обработки выбора подкатегории
-        await query.edit_message_text(text=f"Выбрана {data}")
-        # Вы можете добавить здесь дополнительную логику для каждой подкатегории
+    elif data.startswith('sem_'):
+        await select_subject(update, context, data)
+    # Обработка выбора предмета
+    elif data.startswith('subject_'):
+        selected_subject = data.split('_')[-1].replace('_', ' ')
+        await query.edit_message_text(text=f"Выбран предмет: {selected_subject}")
+
+semester_messages = {
+    'sem_1': "Выберите предмет для первого семестра:\n1. Информатика (Балакин)\n2. Мат. анализ (Филатов)\n3. Линейная алгебра (Судоплатов)\n4. Учебная практика (Коршикова)",
+    'sem_2': "Выберите предмет для второго семестра\n1. Программирование (Балакин, Романов)\n2. Теория вероятности (Зыбарев, Пинигина, Симонов)\n3. Физика (Штыгашев)\n4. Дискретная математика (Судоплатов)\n5. Мат. анализ (Филатов)",
+    'sem_3': "Выберите предмет для третьего семестра\n1. Программирование (Копылова)\n2. Компьютерная графика (Ильиных)\n3. Физика (Штыгашев)\n4. Электротехника (Заякин)",
+    'sem_4': "Выберите предмет для четвертого семестра\n1. Вычислительная математика (Зыбарев, Борин)\n2. Информационные сети (Мищенко)\n3. Компьютерная графика (Дубков)\n4. Моделирование (Альсова)\n5. Операционные системы (Коршикова, Романов)\n6. Технологии и методы программирования (Копылова)\n7. Электроника (Шахтшнайдер)",
+    'sem_5': "Выберите предмет для пятого семестра\n1. Архитектура средств вычислительной техники (Овчеренко)\n2. Базы данных (Трошина, Харюткина)\n3. Основы теории управления (Воевода)\n4. Информационные сети (Перышкова, Скороходов)\n5. Системный анализ (Гошко)\n6. Схемотехника (Гришанов)\n7. Учебная практика (Гошко)\n8. Параллельное программирование (Зеленчук, Малявко)",
+    #'sem_6': "nothing now",
+}
+
+async def select_subject(update: Update, context: ContextTypes.DEFAULT_TYPE, selected_semester: str):
+    if selected_semester == 'sem_1':
+        subjects = ["1", "2", "3", "4"] 
+    elif selected_semester == 'sem_2':
+        subjects = ["1", "2", "3", "4", "5"] 
+    elif selected_semester == 'sem_3':
+        subjects = ["1", "2", "3", "4"] 
+    elif selected_semester == 'sem_4':
+        subjects = ["1", "2", "3", "4", "5", "6", "7"]    
+    elif selected_semester == 'sem_5':
+        subjects = ["1", "2", "3", "4", "5", "6", "7", "8"]    
+    #elif selected_semester == 'sem_6':
+        #subjects = ["я", "не ебу", "какие тут предметы"]    
+    
+    # Получаем индивидуальное сообщение для выбранного семестра
+    message_text = semester_messages.get(selected_semester, "Выберите предмет:")
+
+    keyboard = [
+        [InlineKeyboardButton(subject, callback_data=f"subject_{selected_semester}_{subject.replace(' ', '_')}") for subject in subjects]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text=message_text, reply_markup=reply_markup)
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('smth about our contacts')
+    await update.message.reply_text('Если Вы хотите купить какую-то работу на заказ, либо же хотите задать какие-то вопросы - тут надо бы чото придумать чтобы конф соблюдать ыы')
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
@@ -80,7 +113,7 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     print('Starting bot...')
     app = Application.builder().token(TOKEN).build()
-
+    print('Started.')
 
     # Commands
     app.add_handler(CommandHandler('start', start_command))
@@ -97,3 +130,5 @@ if __name__ == '__main__':
 
     # Polls the bot ?
     app.run_polling(poll_interval = 1)
+
+    
